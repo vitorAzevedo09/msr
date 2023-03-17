2b40971b70d6a7780c6b9df368df9e374b0cd1c9
Vitor Silva <vitor.azevedo009@gmail.com>
2023-03-17 10:02:29 -0300

    Added repository interface for cliente

--- algalog-api/src/main/java/com/algaworks/algalogapi/api/controller/ClienteController.java
+++ algalog-api/src/main/java/com/algaworks/algalogapi/api/controller/ClienteController.java
@@ -2,26 +2,26 @@ package com.algaworks.algalogapi.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalogapi.domain.model.Cliente;
+import com.algaworks.algalogapi.domain.repository.ClienteRepository;
+
+import lombok.AllArgsConstructor;
 
 /**
  * ClienteController
  */
+@AllArgsConstructor
 @RestController
 public class ClienteController {
 
-    @PersistenceContext
-    private EntityManager manager;
+    private ClienteRepository clienteRepository;
 
     @GetMapping("/clientes")
     public List<Cliente> listar() {
-        return manager.createQuery("from Cliente", Cliente.class).getResultList();
+        return clienteRepository.findAll();
     }
 
 }
